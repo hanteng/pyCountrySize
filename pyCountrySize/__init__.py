3acae2abc825a2f44fab65b709fd01903f0f5d44
hanteng <hanteng@gmail.com>
2015-03-07 16:19:53 +0100

    naming: panel reserved for pandas, use sizep instead
    naming: panel reserved for pandas, use sizep instead

--- pyCountrySize/__init__.py
+++ pyCountrySize/__init__.py
@@ -6,7 +6,7 @@ from __future__ import (absolute_import, division, print_function,
 # This is the only place the version is specified and 
 # used in both setup.py and docs/conf.py to set the 
 # version of ggplot.
-__version__ = '0.0.4'
+__version__ = '0.0.4.1'
 
-from .sizedb import (LP, PPPGDP, IPop, IH, IPv4, sizec, panel, meta, description, mapping)
+from .sizedb import (LP, PPPGDP, IPop, IH, IPv4, sizec, sizep, meta, description, mapping)
 
--- pyCountrySize/sizedb/__init__.py
+++ pyCountrySize/sizedb/__init__.py
@@ -5,21 +5,21 @@ from __future__ import (absolute_import, division, print_function,
 import pandas as pd
 import os
 
-__all__ = ["LP","PPPGDP","IPop","IH","IPv4","sizec","panel", "meta", "description", "mapping"]
+__all__ = ["LP","PPPGDP","IPop","IH","IPv4","sizec","sizep", "meta", "description", "mapping"]
 __all__ = [str(u) for u in __all__]
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 from os.path import basename, join, splitext
-panel=pd.read_pickle(os.path.join(_ROOT, "size.pkl"))
-sizec=panel.loc[["IPop","LP","PPPGDP"],:,2013].join(panel.loc[["IH"],:,2012]).join(panel.loc[["IPv4"],:,2015])
+sizep=pd.read_pickle(os.path.join(_ROOT, "size.pkl"))
+sizec=sizep.loc[["IPop","LP","PPPGDP"],:,2013].join(sizep.loc[["IH"],:,2012]).join(sizep.loc[["IPv4"],:,2015])
 mapping=pd.read_pickle(os.path.join(_ROOT, "mapping.pkl")) #mapping['ISO2']['TWN']
 
-LP=sizec['LP']#or panel['LP'][2013] or  panel.loc['LP',:,2013]
-PPPGDP=sizec['PPPGDP']#or panel['PPPGDP'][2013]
-IPop=sizec['IPop']#or panel['IPop'][2013]
-IH=sizec['IH']#or panel['IH'][2012]
-IPv4=sizec['IPv4']#or panel['IPv4'][2015]
+LP=sizec['LP']#or sizep['LP'][2013] or  sizep.loc['LP',:,2013]
+PPPGDP=sizec['PPPGDP']#or sizep['PPPGDP'][2013]
+IPop=sizec['IPop']#or sizep['IPop'][2013]
+IH=sizec['IH']#or sizep['IH'][2012]
+IPv4=sizec['IPv4']#or sizep['IPv4'][2015]
 
 meta={'LP': "Population in 2013 (IMF WEO)",\
       'PPPGDP': "Economy Size in 2013 (IMF WEO)",\
